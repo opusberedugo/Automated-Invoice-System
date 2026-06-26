da6724a41bcabb5ebd0ddea4198562aead7215ea
opusberedugo <opusberedugo@gmail.com>
2026-06-26 23:07:07 +0400

    feat: replace netlify.toml with express serverless function implementation

--- netlify/functions/api.cjs
+++ netlify/functions/api.cjs
@@ -7,6 +7,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const fs = require('fs');
 const path = require('path');
